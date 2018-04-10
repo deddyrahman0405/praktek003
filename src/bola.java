9ef9385a0aaad79f7144935ab23bebf9e1d686a5
deddyrahman0405 <deddrahman0405kodokgaul023@gmail.com>
2018-04-09 21:16:38 -0700

    buat getter and setter

--- src/bola.java
+++ src/bola.java
@@ -20,4 +20,38 @@ public class bola {
                 "warna \t: "+warna+"\n"+
                 "harga \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+     
+     
 }
