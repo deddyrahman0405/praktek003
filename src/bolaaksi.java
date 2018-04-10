9d0b7e39896c41b20a79a2f9ff3f0fe16df0a187
deddyrahman0405 <deddrahman0405kodokgaul023@gmail.com>
2018-04-09 21:24:48 -0700

    menggunakan method get

--- src/bolaaksi.java
+++ src/bolaaksi.java
@@ -19,5 +19,15 @@ public class bolaaksi {
         b.setHarga(200000);
         
         b.cetakinfo();
+        
+        System.out.print("merknya \t: ");
+        System.out.println(b.getMerk());
+        System.out.print("ukurannya \t: ");
+        System.out.println(b.getUkuran());
+        System.out.print("warnanya \t: ");
+        System.out.println(b.getWarna());
+        System.out.print("harganya \t: ");
+        System.out.println(b.getHarga());
+        
     }
 }
